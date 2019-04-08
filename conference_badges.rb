c784a65f98a147b6739f1ac969831e0a076fba10
Jennifer Kim <jjkim429@gmail.com>
2019-04-08 21:34:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  array.each do |x|
+    return "Hello, my name is #{x}."
+  end
+end
+
+def assign_rooms(speakers)
+end
